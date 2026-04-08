03848951cdfd6a22ccf3ee0f504b0060ab85f681
Ayush442842q <pratyushextra123456@gmail.com>
2026-04-08 23:54:28 +0530

    day02: add test for password length

---
+++ day02-password-generator/tests/test_generator.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(".."))
+
+from generator import generate_password, validate_length, check_password_strength
+
+def test_password_length():
+    password = generate_password(12)
+    assert len(password) == 12
+    print("✅ test_password_length passed!")
+
+test_password_length()
