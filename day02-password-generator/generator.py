74d4e076aff7ed4465294b2eae6504cc57cddcab
Ayush442842q <pratyushextra123456@gmail.com>
2026-04-08 23:25:31 +0530

    day02: add generate_password core function

--- day02-password-generator/generator.py
+++ day02-password-generator/generator.py
@@ -6,3 +6,16 @@ LOWERCASE = string.ascii_lowercase
 UPPERCASE = string.ascii_uppercase
 DIGITS = string.digits
 SYMBOLS = "!@#$%^&*()_+-=[]{}|;:,.<>?"
+def generate_password(length=12, use_upper=True, use_digits=True, use_symbols=True):
+    """Generates a random password with given options."""
+    characters = LOWERCASE
+
+    if use_upper:
+        characters += UPPERCASE
+    if use_digits:
+        characters += DIGITS
+    if use_symbols:
+        characters += SYMBOLS
+
+    password = "".join(random.choice(characters) for _ in range(length))
+    return password
