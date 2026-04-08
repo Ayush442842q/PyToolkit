7d565a3f70590dccfd5479a38fb08408da10069e
Ayush442842q <pratyushextra123456@gmail.com>
2026-04-08 23:22:42 +0530

    day02: add character sets for password generation

---
+++ day02-password-generator/generator.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+# Character sets
+LOWERCASE = string.ascii_lowercase
+UPPERCASE = string.ascii_uppercase
+DIGITS = string.digits
+SYMBOLS = "!@#$%^&*()_+-=[]{}|;:,.<>?"
