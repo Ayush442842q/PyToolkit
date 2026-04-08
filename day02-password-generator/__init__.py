027289146635d53b5a991d9213988a9ce9f1e766
Ayush442842q <pratyushextra123456@gmail.com>
2026-04-09 00:37:36 +0530

    day02: add __init__.py for package structure

---
+++ day02-password-generator/__init__.py
@@ -0,0 +1,4 @@
+# Day 02 - Password Generator
+# Generates secure random passwords with custom options
+
+from .generator import generate_password, validate_length, check_password_strength, generate_multiple_passwords
