a2c99496c160bbc1ccf3351b5356039797e2c101
Ayush442842q <pratyushextra123456@gmail.com>
2026-04-09 20:58:24 +0530

    Add __init__.py to expose weather module functions

---
+++ day03-weather-cli/__init__.py
@@ -0,0 +1,13 @@
+from .weather import (
+    get_weather,
+    parse_weather,
+    display_weather,
+    get_forecast,
+    display_forecast,
+    compare_cities,
+    convert_temperature,
+    get_weather_with_unit
+)
+
+__version__ = "1.0.0"
+__author__ = "Ayush442842q"
