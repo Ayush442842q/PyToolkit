4c563444b540a82b16580a186d9b13503fb2ca2f
Ayush <pratyushextra123456@gmail.com>
2026-04-12 06:35:59 +0530

    day06: add __init__.py for package structure

---
+++ day06-pdf-merger-splitter/__init__.py
@@ -0,0 +1,16 @@
+# Day 06 - PDF Merger/Splitter
+# Author: Ayush442842q
+
+from .pdf_tool import (
+    merge_pdfs,
+    split_pdf,
+    get_pdf_info,
+    extract_pages,
+    display_info,
+    rotate_pages,
+    add_watermark,
+    get_page_count
+)
+
+__version__ = "1.0.0"
+__author__ = "Ayush442842q"
