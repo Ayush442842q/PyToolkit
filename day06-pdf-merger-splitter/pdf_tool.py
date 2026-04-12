e6129a60031be3e4423d8835ce75f75b5a5af4dc
Ayush <pratyushextra123456@gmail.com>
2026-04-12 06:23:24 +0530

    day06: add imports and configuration

---
+++ day06-pdf-merger-splitter/pdf_tool.py
@@ -0,0 +1,7 @@
+import os
+from PyPDF2 import PdfReader, PdfWriter
+
+# PyToolkit - Day 06
+# Tool: PDF Merger/Splitter
+# Author: Ayush442842q
+# Description: Merge, split, rotate and extract pages from PDF files
