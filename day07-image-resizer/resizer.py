0e3e4403b87d9514dd3aa633344f7d68c432b9b5
Ayush <pratyushextra123456@gmail.com>
2026-04-15 04:12:26 +0530

    day07: add imports and configuration

---
+++ day07-image-resizer/resizer.py
@@ -0,0 +1,9 @@
+import os
+from PIL import Image
+
+# PyToolkit - Day 07
+# Tool: Image Resizer
+# Author: Ayush442842q
+# Description: Resize, crop, convert and apply filters to images
+
+SUPPORTED_FORMATS = [".jpg", ".jpeg", ".png", ".bmp", ".gif", ".webp"]
