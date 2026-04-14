8b79707606512a0925023cb5e131e602f0d39d77
Ayush <pratyushextra123456@gmail.com>
2026-04-15 04:49:30 +0530

    day08: add __init__.py for package structure

---
+++ day08-url-shortener/__init__.py
@@ -0,0 +1,17 @@
+# Day 08 - URL Shortener
+# Author: Ayush442842q
+
+from .shortener import (
+    is_valid_url,
+    shorten_url,
+    shorten_bulk,
+    load_history,
+    save_history,
+    add_to_history,
+    display_history,
+    clear_history,
+    get_history_count
+)
+
+__version__ = "1.0.0"
+__author__ = "Ayush442842q"
