e019d63984843f91f76f064c4c8ae5c8b66a6997
Ayush <pratyushextra123456@gmail.com>
2026-04-15 04:30:32 +0530

    day07: add __init__.py for package structure

---
+++ day07-image-resizer/__init__.py
@@ -0,0 +1,18 @@
+# Day 07 - Image Resizer
+# Author: Ayush442842q
+
+from .resizer import (
+    get_image_info,
+    display_info,
+    resize_image,
+    resize_by_percentage,
+    crop_image,
+    convert_format,
+    apply_grayscale,
+    apply_blur,
+    rotate_image,
+    flip_image
+)
+
+__version__ = "1.0.0"
+__author__ = "Ayush442842q"
