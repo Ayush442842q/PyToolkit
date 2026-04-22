630777e9361853dc12c5d5d6ce8a1349e3d151eb
Ayush <a97998639@gmail.com>
2026-04-22 18:58:19 +0530

    day09: add __init__.py for package structure

---
+++ day09-email-sender/__init__.py
@@ -0,0 +1,15 @@
+# Day 09 - Email Sender
+# Author: Ayush442842q
+
+from .sender import (
+    is_valid_email,
+    create_message,
+    add_attachment,
+    send_email,
+    send_bulk_emails,
+    log_email,
+    view_log
+)
+
+__version__ = "1.0.0"
+__author__ = "Ayush442842q"
