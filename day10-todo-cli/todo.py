30bf4bec6701e051bdcfb4272a1615d6bc6e4d8d
Ayush <a97998639@gmail.com>
2026-04-24 15:12:07 +0530

    day10: add imports and configuration

---
+++ day10-todo-cli/todo.py
@@ -0,0 +1,10 @@
+import json
+import os
+from datetime import datetime
+
+# PyToolkit - Day 10
+# Tool: Todo CLI App
+# Author: Ayush442842q
+# Description: A full-featured command-line Todo app with JSON storage
+
+TODO_FILE = "todos.json"
