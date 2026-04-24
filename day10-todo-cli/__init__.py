2d66b4a44beab78d3ae7c6d5f9585ad020608f3a
Ayush442842q <pratyushextra123456@gmail.com>
2026-04-24 15:59:35 +0530

    day10: add __init__.py for package structure

---
+++ day10-todo-cli/__init__.py
@@ -0,0 +1,18 @@
+# Day 10 - Todo CLI App
+# Author: Ayush442842q
+
+from .todo import (
+    load_todos,
+    save_todos,
+    add_todo,
+    list_todos,
+    mark_done,
+    delete_todo,
+    edit_todo,
+    search_todos,
+    get_stats,
+    clear_done
+)
+
+__version__ = "1.0.0"
+__author__ = "Ayush442842q"
